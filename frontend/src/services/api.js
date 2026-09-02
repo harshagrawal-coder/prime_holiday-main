1db71d31820ee45948985c320e44bad8e10d65a9
Harsh Agrawal <harshagrawal9498@gmail.com>
2026-09-02 15:45:17 +0530

    updates

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,20 +1,16 @@
 import axios from "axios";
-
 const api = axios.create({
   baseURL: "https://prime-holiday-main.onrender.com/api",
 });
-
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+   config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
-
 api.interceptors.response.use(
   (response) => response,
   (error) => Promise.reject(error),
 );
-
 export default api;
